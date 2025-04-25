aa0fde972815fc4337518cf50e26a184570bf97c
HasnainRaza16 <bhaihassi317@gmail.com>
2025-04-26 01:38:03 +0500

    Update project.c
    1st done...

--- project.c
+++ project.c
@@ -1,1 +1,15 @@
+void saveMembersToFile() {
+    FILE *file = fopen(FILE_NAME, "w");
+    if (!file) {
+        printf("Error: Unable to save members to file.\n");
+        return;
+    }
 
+    for (int i = 0; i < memberCount; i++) {
+        fprintf(file, "%d,%s,%s,%d,%d,%d,%s\n",
+                members[i].id, members[i].name, members[i].membershipType,
+                members[i].feesPaid, members[i].attendance,
+                members[i].paymentStatus, members[i].feedback);
+    }
+    fclose(file);
+}
